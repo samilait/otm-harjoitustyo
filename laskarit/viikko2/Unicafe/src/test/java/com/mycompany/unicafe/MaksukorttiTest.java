f7eb5c44837546078b526fb731ead41a23555eda
samilait <sami.p.laitinen@gmail.com>
2018-03-27 22:44:37 +0300

    Laskarit viikko2, jäi kassapäätetestiä vaille valmiiksi

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,34 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void saldoOnOikein() {
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+    
+    @Test
+    public void rahanLataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(10);
+        assertEquals("saldo: 20.0", kortti.toString());
+    }
+
+    @Test
+    public void saldoVaheneeOikein() {
+        kortti.otaRahaa(5);
+        assertEquals("saldo: 5.0", kortti.toString());
+    }
+
+    @Test
+    public void saldoEiMuutuJosEiKatetta() {
+        kortti.otaRahaa(15);
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+
+    @Test
+    public void otaRahaaPalauttaaOikein() {
+        kortti.otaRahaa(15);
+        assertEquals(false, kortti.otaRahaa(15));
+    }
+
 }
